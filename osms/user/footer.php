fa0ecea8159a29a80d1b1e604ed83039764819c1
MrNegative001 <anasyusufjallabiya@gmail.com>
2023-10-23 20:33:27 +0100

    I just edited the footer

--- osms/user/footer.php
+++ osms/user/footer.php
@@ -4,9 +4,18 @@
 	color: #000000;
 	font-weight: bold;
 }
+footer {
+            background-color: #333;
+            color: white;
+            text-align: center;
+            padding: 10px;
+            position: fixed;
+            bottom: 0;
+            width: 100%;
+        }
 -->
 </style>
- <div class="footer">
-          <div class="style1"> <p> All Rights Reserved. &copy; 2021  Designed By : S Ayuba </p>
+ <footer class="footer">
+          <div class="style1"> <p> All Rights Reserved. &copy; 2023  Designed By : Yusuf Anas julee NAS/STE/19/1068</p>
 </div>
-</div>
+</footer>
